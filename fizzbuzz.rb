8b8f0e5ffbcac8fdeb33da698be57971bf629d68
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-05 00:29:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(n)
   if n % 3 == 0
-#    if n % 5 == 0
-#      puts "FizzBuzz"
-#    else puts "Fizz"
-#    end
-#  elseif n % 5 == 0
-#    puts "Buzz"
-  puts "Fizz"
+    if n % 5 == 0
+      string = "FizzBuzz"
+    else string = "Fizz"
+    end
+  elseif n % 5 == 0
+    string "Buzz"
   end
 end
